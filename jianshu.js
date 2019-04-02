acc88f5f4284428f5ed010e374dce5e0660e3a0a
Maggie.yang <Maggie.Yang@royaleinternational.com>
2019-04-02 18:07:51 +0800

    day 0402

--- jianshu.js
+++ jianshu.js
@@ -1,25 +1,48 @@
 const cheerio = require("cheerio");
 const http = require("http");
+const async = require("async")
 const request = require("request")
 let url = 'https://www.jianshu.com/c/NEt52a?order_by=top&page=';
 let domin = "https://www.jianshu.com"
-const getList = (i) => {
+
+const getList = (listUrls, leakUrl) => {
     return new Promise((resolve, reject) => {
-        request(url + i, (err, response, body) => {
-            if (err) reject(err)
-            resolve({
-                statusCode: response.statusCode,
-                content: body
+        async.mapLimit(listUrls, 2, (url, callback) => {
+            request(url, (err, response, body) => {
+                // 有遗漏
+                !body && leakUrl.push(url)
+                let $ = cheerio.load(body);
+                let res = []
+                Array.from($(".note-list li .title")).forEach(x => {
+                    res.push($(x).attr("href"))
+                })
+                callback(null, res)
+
             })
+        }, (err, result) => {
+            if(err) reject(err)
+            let res = []
+            result.forEach(x => {
+                res = res.concat(x)
+            })
+            res = [...new Set(res)];
+            resolve({res, leakUrl})
         })
     })
+
 }
-let times = 0
-// let promise = Promise.all([getList(1),getList(2),getList(3),getList(4)]).then(data => {
-//     console.log(data)
-// })
+
+
+let listUrls = Array.from({ length: 3 }, (x, i) => url + (i + 1));
+let leakUrl = []
+getList(listUrls,leakUrl).then(res => {
+    console.log(res);
+    // if(res.leakUrl.length)
+})
+
+
 const f1 = () => {
-    console.time("begin")
+    console.time("f1")
     new Promise((resolve, reject) => {
         const urls = []
         request('https://www.jianshu.com/c/NEt52a?order_by=top', async (err, respense, body) => {
@@ -56,15 +79,15 @@ const f1 = () => {
             )
         })
         .then(results => {
-            console.timeEnd("begin")
+            console.timeEnd("f1")
             // console.log(results)
         })
 }
 
-f1();
+// f1();
 const f2 = () => {
     let res = [];
-    console.time("end")
+    console.time("f2")
     request('https://www.jianshu.com/c/NEt52a?order_by=top', async (err, respense, body) => {
         let $ = cheerio.load(body);
         let len = $(".note-list li").length;
@@ -93,10 +116,42 @@ const f2 = () => {
         }
         Promise.all(res).then(data => {
             // console.log(data);
-            console.timeEnd("end")
+            console.timeEnd("f2")
         })
     })
 
 }
-f2();
+// f2();
+
+const f3 = () => {
+    let subUrl = [];
+    console.time("f3")
+    new Promise((resolve, reject) => {
+        request("https://www.jianshu.com/c/NEt52a?order_by=top", async (err, response, body) => {
+            if (err) console.log(err);
+            let $ = cheerio.load(body);
+            let len = $(".note-list li").length;
+            for (let i = 0; i < len; i++) {
+                let cur = $(".note-list li")[i]
+                subUrl.push($(cur).find("a").attr("href"));
+            }
+            resolve(subUrl)
+        })
+    }).then(urls => {
+        // 最多有5个请求同时发   一个完成了就会再加一个
+        console.time("async")
+        async.mapLimit(urls, 5, (url, callback) => {
+            // console.log(url)
+            request(domin + url, (err, response, body) => {
+                callback(null, body)
+            })
+        }, function (err, result) {
+            console.timeEnd("async")
+            //    console.log("result", result)
+        })
+    })
+}
+// f3();
+
+
 
